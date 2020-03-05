a6c0d84fdd3ecc699ad49c9dbeef369aef87c334
jackchensm <shaodiandian88@163.com>
2020-03-05 10:30:25 +0800

    test(queue):测试顺序队列实现
    使用数组实现的顺序集中存储【顺序存储结构】
    从队尾进队时
    需要判断队列是否已满
    从队头出队时
    需要判断队列是否为空队

--- datastruct/cmake-build-debug/CMakeFiles/line.dir/DependInfo.cmake
+++ datastruct/cmake-build-debug/CMakeFiles/line.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "E:/c/linux/datastruct/linkStack.c" "E:/c/linux/datastruct/cmake-build-debug/CMakeFiles/line.dir/linkStack.c.obj"
+  "E:/c/linux/datastruct/queue2.c" "E:/c/linux/datastruct/cmake-build-debug/CMakeFiles/line.dir/queue2.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 

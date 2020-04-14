0d0b5c5187ac8721efc002078b9cc3c5478dde02
jackchensm <shaodiandian88@163.com>
2020-04-14 17:44:26 +0800

    code(c++):类中的成员方法/函数与内联函数
    内联函数不在是函数，更像是宏带参数的替换
    
    c++中的类中的成员方法默认就是内联函数，所以
    在类休外最好定义一个函数，而类体内只声明成员方法使用

--- test/demo13.cpp
+++ test/demo13.cpp
@@ -13,26 +13,32 @@ public:
     char *name;
     int age;
     int score;
-    void display()
+    void display()//类体中的函数默认为内联函数
     {
-        cout<<name<<"'s age is "<<age<<",score is "<<score;
+        cout<<name<<"'s age is "<<age<<",score is "<<score<<endl;
     }
+    void talk();//类体中声明
 };
+void person::talk(){//类体外里定义  不让它是内联函数  内联函数类似宏替换 
+    cout<<"talk method"<<name<<"'s age is "<<age<<",score is "<<score<<endl;
+}
 int main()
 {
 
-    person tom;//分配到栈区
-    tom.name = "tom";
-    tom.age = 100;
-    tom.score=100;
-    tom.display();
 
-    person allpeople[2];
-    allpeople[0].name="lucy";
-    allpeople[0].age=100;
-    allpeople[0].score=90;
 
-    allpeople[0].display();
+//    person tom;//分配到栈区
+//    tom.name = "tom";
+//    tom.age = 100;
+//    tom.score=100;
+//    tom.display();
+//
+//    person allpeople[2];
+//    allpeople[0].name="lucy";
+//    allpeople[0].age=100;
+//    allpeople[0].score=90;
+//
+//    allpeople[0].display();
 
 
     person *jack = new person;//分配到堆区
@@ -40,6 +46,9 @@ int main()
     jack->score=100;
     jack->age=200;
     jack->display();
+    jack->talk();
+
+
 
     delete jack;
     return 0;

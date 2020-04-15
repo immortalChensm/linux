3cdbec5036daeb1a408abb134641c52096b1701b
jackchensm <shaodiandian88@163.com>
2020-04-15 14:31:05 +0800

    code(c++):c++构造函数重载

--- test/demo18.cpp
+++ test/demo18.cpp
@@ -14,14 +14,41 @@ private:
     int m_score;
     char *m_name;
 public:
+    Person();//构造函数重载
     Person(int age,int score,char *name);//构造函数
+    void setAge(int age);
+    void setScore(int score);
+    void setName(char *name);
+    void show();
     void display();
 };
 
+Person::Person() {
+    m_score = 0;
+    m_age = 0;
+    m_name = NULL;
+}
 Person::Person(int age, int score, char *name) {
     m_name = name;
     m_score = score;
     m_age = age;
+}
+void Person::setName(char *name) {
+    m_name = name;
+}
+void Person::setScore(int score) {
+    m_score = score;
+}
+void Person::setAge(int age) {
+    m_age = age;
+}
+void Person::show() {
+    if (m_score==0||m_name==NULL){
+        cout<<"class property of value is empty\n";
+    }else{
+        cout<<"show method "<<m_name<<m_age<<m_score<<endl;
+    }
+
 }
 void Person::display() {
     cout<<m_name<<" 's age is "<<m_age<<" and score is "<<m_score<<endl;
@@ -35,5 +62,16 @@ int main()
 
     Person jack(200,60,(char*)"jack");
     jack.display();
+
+    Person lucy;
+    lucy.show();
+    Person chen(100,90,(char*)"chen");
+    chen.show();
+
+    Person *tang = new Person();
+    tang->setAge(100);
+    tang->setScore(60);
+    tang->setName((char*)"tang");
+    tang->show();
     return 0;
 }

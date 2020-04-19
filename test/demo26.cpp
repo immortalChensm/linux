91e3c39488b1f2a549b7e12c838fa5623136034a
jackchensm <shaodiandian88@163.com>
2020-04-19 15:40:53 +0800

    code(c++):封闭类的构造函数和析构函数运行时机

--- test/demo26.cpp
+++ test/demo26.cpp
@@ -13,10 +13,15 @@ private:
     int m_area;
 public:
     House(char *address,int area);
+    ~House(){
+        cout<<"house destrcuctor"<<endl;
+    }
     void show()const;
 };
 
-House::House(char *address, int area):m_address(address),m_area(area) {}
+House::House(char *address, int area):m_address(address),m_area(area) {
+    cout<<"house constructor\n";
+}
 void House::show() const {
     cout<<"house address is:"<<this->m_address<<",area is "<<this->m_area<<endl;
 }
@@ -28,16 +33,22 @@ private:
     House m_house;
 public:
     MarketHouse(int price, char *address,int area);
+    ~MarketHouse(){
+        cout<<"marethouse destructor\n";
+    }
     void show() const;
 };
 
-MarketHouse::MarketHouse(int price, char *address,int area):m_price(price),m_house(address,area) {}
+MarketHouse::MarketHouse(int price, char *address,int area):m_price(price),m_house(address,area) {
+    cout<<"markethouse constructor\n";
+}
 void MarketHouse::show() const {
     cout<<"marketableHousing price is:"<<this->m_price<<endl;
     this->m_house.show();
 }
 int main()
 {
+    //在栈上创建一个对象  并初始化成员
     MarketHouse house(100,(char*)"beijing",300);
     house.show();
 
